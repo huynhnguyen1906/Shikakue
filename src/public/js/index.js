8093130600f2fee2e53abe29143a2fbe0e3e4d46
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-01-17 19:14:34 +0900

    Update package.json and index.js

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,19 +1,21 @@
-const status = document.querySelector(".progress_bar")
-const active = document.querySelector(".inner_bar")
-const statusText = document.querySelector(".status-text")
+document.addEventListener("DOMContentLoaded", (event) => {
+	const status = document.querySelector(".progress_bar")
+	const active = document.querySelector(".inner_bar")
+	const statusText = document.querySelector(".status-text")
 
-let value = sessionStorage.getItem("correctAnswer")
-if (value === null) {
-	value = 0
-} else {
-	value = parseInt(value)
-}
+	let value = sessionStorage.getItem("correctAnswer")
+	if (value === null) {
+		value = 0
+	} else {
+		value = parseInt(value)
+	}
 
-if (value >= 30) {
-	active.style.width = "100%"
-	statusText.innerText = "30問+"
-	active.style.borderRadius = "40px"
-} else {
-	active.style.width = (value * 100) / 30 + "%"
-	statusText.innerText = value + "問"
-}
+	if (value >= 30) {
+		active.style.width = "100%"
+		statusText.innerText = "30問+"
+		active.style.borderRadius = "40px"
+	} else {
+		active.style.width = (value * 100) / 30 + "%"
+		statusText.innerText = value + "問"
+	}
+})
