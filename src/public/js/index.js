7c4ba593c94c784621e16a2066598b82fde33aff
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-01-19 10:43:00 +0900

    Update goal number dynamically

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,8 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-	const status = document.querySelector(".progress_bar")
+	function isNumber(value) {
+		return !isNaN(value) && value !== null && value !== ""
+	}
+
+	let goal = sessionStorage.getItem("goal")
+	if (goal === null) {
+		for (let i = 0; i < Infinity; i++) {
+			goal = prompt("1日の目標問数を入力してください。")
+			if (isNumber(goal)) {
+				sessionStorage.setItem("goal", goal)
+				break
+			} else {
+				alert("数値だけ入力してください！")
+			}
+		}
+	}
+
 	const active = document.querySelector(".inner_bar")
 	const statusText = document.querySelector(".status-text")
-
+	const goalNum = document.querySelector(".goal-num")
+	goalNum.innerText = goal
 	let value = sessionStorage.getItem("correctAnswer")
 	if (value === null) {
 		value = 0
@@ -10,12 +27,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		value = parseInt(value)
 	}
 
-	if (value >= 30) {
+	if (value >= goal) {
 		active.style.width = "100%"
-		statusText.innerText = "30問+"
+		statusText.innerText = "目標達成"
+		statusText.style.color = "#3D8D00"
 		active.style.borderRadius = "40px"
+		active.style.backgroundColor = "#3DED39"
 	} else {
-		active.style.width = (value * 100) / 30 + "%"
+		active.style.width = (value * 100) / goal + "%"
 		statusText.innerText = value + "問"
 	}
 })
